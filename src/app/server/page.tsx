08fa20623e67040f9bf2f63981835e8e46db8125
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-06-30 14:33:31 +0530

    trpc

---
+++ src/app/server/page.tsx
@@ -0,0 +1,13 @@
+import { caller } from '@/trpc/server'
+import React from 'react'
+
+const page = async() => {
+    const data =await caller.hello({text: 'ssr'})
+  return (
+    <div>
+        {JSON.stringify(data)}
+    </div>
+  )
+}
+
+export default page
